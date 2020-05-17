96246cce296281995ee5e426da8cd6e8f6b539cb
ferirog <ferirog@gmail.com>
2020-05-17 23:30:27 +0700

    Styling with Styled Components

--- react-tetris/src/components/Cell.js
+++ react-tetris/src/components/Cell.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import {StyledCell} from './styles/StyledCell';
+import {TETROMINOS} from '../tetrominos';
 
 const Cell = ({ type }) => (
-    <div>Cell</div>
+    //Tampilin block cell dengan warna ngikutin komponen TETROMINOS
+    <StyledCell type={type} color={TETROMINOS[type].color} />
 )
 
 export default Cell;
--- react-tetris/src/components/Stage.js
+++ react-tetris/src/components/Stage.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import {StyledStage} from './styles/StyledStage';
+
 import Cell from './Cell';
 
 // We have the stage that's the one we created
@@ -11,9 +13,9 @@ import Cell from './Cell';
 // For each cell we are going to render out the cell component
 
 const Stage = ({ stage }) => (
-    <div>
-        {stage.map(row => row.map((cell, x) => <Cell key={x} type={cell[0]} /> ))}
-    </div>
+    <StyledStage width={stage[0].length} height={stage.length}>
+        {stage.map(row => row.map((cell, x) => <Cell key={x} type={cell[0]} />))}
+    </StyledStage>
 )
 
 export default Stage;
